bd96989255aca81542570afa82c26bc8b0f5f867
José David Rodriguez Payo <josedavidrp12@gmail.com>
2025-07-02 19:46:14 +0200

    Implementación de primevue, primeicons, primeflex

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,20 @@
 import { createApp } from 'vue'
+import PrimeVue from 'primevue/config'
+import 'primeicons/primeicons.css'
+import 'primeflex/primeflex.css'
+import Button from 'primevue/button'
+import Material from '@primeuix/themes/material';
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App) // ← this is your Vue app instance
+
+app.use(PrimeVue, {
+    // Default theme configuration
+    theme: {
+        preset: Material
+    }
+ });
+app.component('Button', Button)
+
+app.mount('#app')
